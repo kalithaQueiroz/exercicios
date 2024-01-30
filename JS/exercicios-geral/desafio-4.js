8729d79b93c46a4134cc778cb5acb28ceb6e26e6
kalithaQueiroz <kalitaqueiroz@gmail.com>
2024-01-30 16:30:07 -0300

    add desafio-4

---
+++ JS/exercicios-geral/desafio-4.js
@@ -0,0 +1,39 @@
+// Elabore um algoritmo que dado peso e altura de um adulto mostre sua condição de acordo com a tabea abaixo.
+
+// 1 - Abaixo de 18.5 (Abaixo do peso);
+// 2 - Entre 18.5 e 25 (Peso normal);
+// 3 - Entre 25 e 30 (Acima do peso);
+// 4 - Entre 30 e 40 (Obeso);
+// 5 - Acima de 40 (Obesidade Grave);
+
+const readline = require('readline');
+const rl = readline.createInterface({
+    input: process.stdin,
+    output: process.stdout
+});
+
+rl.question('Digite o peso: ', (peso) => {
+    rl.question('Digite a altura: ', (altura) => {
+
+        // peso = parseInt(peso);
+        // altura= parseInt(altura);
+
+        const imc = (peso / Math.pow(altura, 2)).toFixed(2);
+        
+        if (imc < 18.5) {
+            status = `Abaixo do peso`
+        } else if (imc >= 18.5 && imc <= 25) {
+            status = `no Peso normal`
+        } else if (imc >= 25 && imc <= 30) {
+            status = `Acima do peso`
+        } else if (imc >= 30 && imc <= 40) {
+            status = `Obeso`
+        } else {
+            status = `com Obesidade Grave`
+        }
+        
+        console.log(`A IMC é ${imc}, e essa pessoa está ${status}.`);
+
+        rl.close();
+    });
+});
