e852bfd10f1dc6b313eeb90213d7fa050a96e58a
kalithaQueiroz <kalitaqueiroz@gmail.com>
2024-02-05 16:29:04 -0300

    updating desafio-9

--- JS/exercicios-geral/desafio-9.js
+++ JS/exercicios-geral/desafio-9.js
@@ -1,13 +1,12 @@
+// Crie um programa que dado notas imprima a média.
+
 const notas = [];
 
-notas.push(2);
+notas.push(5);
 notas.push(7);
 notas.push(9);
-notas.push(5);
-notas.push(3);
 
 let soma = 0;
-
 
 for (let i = 0; i < notas.length; i++) {
     const nota = notas[i];
